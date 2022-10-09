2b07e5b3e4f77ea6a28221e965ad4983c14e89f9
vyshak-12918 <vyshak.puthusseri@zohocorp.com>
2022-10-09 19:50:56 +0530

    feat: Add the base class for the neo4j connection.
    We can use this as a wrapper written over the python driver for neo4j.
    Create an object of this class and invoke the query() method with
    necessary parameters.

---
+++ Neo4jConnection.py
@@ -0,0 +1,37 @@
+from neo4j import GraphDatabase
+class Neo4jConnection:
+    def __init__(self, url, username, password):
+        self.__url = url
+        self.__username = username
+        self.__password = password
+        self.__driver = None
+        try:
+            self.__driver = GraphDatabase.driver(self.__url, auth=(self.__username, self.__password))
+        except Exception as e:
+            print("Not able to connect to GraphDatabase", e)
+        
+    def close(self):
+        if self.__driver is not None:
+            self.__driver.close()
+        
+    def query(self, query, parameters=None, db=None):
+        assert self.__driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try: 
+            session = self.__driver.session(database=db) if db is not None else self.__driver.session() 
+            response = list(session.run(query, parameters))
+        except Exception as e:
+            print("Query failed:", e)
+        finally: 
+            if session is not None:
+                session.close()
+        return response
+
+
+# conn = Neo4jConnection(url="bolt://localhost:7687", username="neo4j", password="")
+# print(type(conn))
+# qu = "match (n:Person{born:1996}) return n"
+# result = conn.query(qu)
+# print(result)
+# conn.close()
